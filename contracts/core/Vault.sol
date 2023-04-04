720fc0ac01e61c06ccb9ac46022965c306314a14
Love Trading <lovetrading09@yahoo.com>
2023-04-04 14:04:58 -0500

    remove confirm delay for add position

--- contracts/core/Vault.sol
+++ contracts/core/Vault.sol
@@ -105,7 +105,7 @@ contract Vault is Constants, ReentrancyGuard, Ownable, IVault {
         require(operators.getOperatorLevel(msg.sender) >= uint8(1), "Invalid operator");
         // put a require here to call something like positionVault.getPositionProfit(_posId)
         // compare to maxProfitPercent and totalUSD, if the position profit > max profit % of totalUSD, close
-        (Position memory position, , ) = positionVault.getPosition(_posId);
+        (Position memory position,) = positionVault.getPosition(_posId);
         uint256 price = priceManager.getLastPrice(position.indexToken);
         (bool isProfit, uint256 pnl) = settingsManager.getPnl(
             position.indexToken,
@@ -134,7 +134,7 @@ contract Vault is Constants, ReentrancyGuard, Ownable, IVault {
     }
 
     function _closePosition(uint256 _posId) internal {
-        (Position memory pos, , ) = positionVault.getPosition(_posId);
+        (Position memory pos,) = positionVault.getPosition(_posId);
         positionVault.decreasePosition(msg.sender, pos.size, _posId);
     }
 
